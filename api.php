f598548cec9d73c20097cd3d69e58202c1ec640e
JorgeArmandoSB <jorgesilvacreativo@gmail.com>
2022-08-05 18:59:51 -0500

    calculadora datos pedidos al php

--- api.php
+++ api.php
@@ -1,26 +1,27 @@
 <?php
-    
-    header('Access-Control-Allow-Origin: *');
+    header("Access-Control-Allow-Origin: *");
     header("Content-Type: application/json");
-    // print_r(json_encode((object) [
-    //     "Mensaje" => (string) "Realizar los operadores de la calculadora",
-    //     "Servidor" => $_SERVER["HTTP_HOST"]
-    // ], JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_HEX_TAG));
-
-    $_DATA = file_get_contents("php://input");
-    $_DATA = json_decode($_DATA, true);
+    $_DATA = ($_SERVER["REQUEST_METHOD"] == "POST") 
+                ? json_decode(file_get_contents("php://input"), true)
+                : (array) ["num1"=> 0 , "num2" => 0, "operador" => null];
     extract($_DATA);
+    unset($_DATA);
 
-    
-    var_dump("la respuesta es: ". $_DATA["ans"] . " "  .$_SERVER['HTTP_HOST']);
-   
+    $res = match($operador){
+        "suma" => (float)$num1 + (float)$num2,
+        "resta" => (float)$num1 - (float)$num2,
+        "multiplicacion" => (float)$num1 * (float)$num2,
+        "divicion" =>  ((float)$num2 == 0) ? 0 : (float)$num1 / (float)$num2,
+        default => 0
+    };
 
-    
 
-   
+    print_r(json_encode((object) [
+        "Mensaje" => (string) "Realizar los operadores de la calculadora",
+        "Servidor" => $_SERVER["HTTP_HOST"],
+        "Operacion" => "el resultado de la $operador es: $res",
+        "Resultado" => $res
+    ], JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_HEX_TAG))
 
-   
-  
-  
 
 ?>
